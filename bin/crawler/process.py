6f1cfc21e06b5ad06464d4678b02b2533136478d
Pomin Wu <pomin5@gmail.com>
2017-02-18 09:38:44 -0500

    Fixed sleeping between tasks

--- bin/crawler/process.py
+++ bin/crawler/process.py
@@ -10,17 +10,21 @@ def do_some(data, task, updated,
             nochange_message='{item} nochange',
             failed_message='{item} failed',
             logger=default_logger):
-    for item in data:
+    count = 0
+    for i, item in enumerate(data):
         try:
             r = task(item)
             if updated(r):
                 logger.info(updated_message.format(item=item))
-                time.sleep(sleep_sec)
+                count = count + 1
+                if i < len(data) - 1:
+                    time.sleep(sleep_sec)
             else:
                 logger.info(nochange_message.format(item=item))
         except Exception as e:
             logger.warn(failed_message.format(item=item))
             logger.debug(e)
+    return count
 
 
 def do_batch(paginate, task, updated,
@@ -31,8 +35,9 @@ def do_batch(paginate, task, updated,
              logger=default_logger):
     for data in paginate:
         time.sleep(sleep_sec)
-        do_some(data, task, updated,
-                updated_message=updated_message,
-                nochange_message=nochange_message,
-                failed_message=failed_message)
-        time.sleep(sleep_sec)
+        r = do_some(data, task, updated,
+                    updated_message=updated_message,
+                    nochange_message=nochange_message,
+                    failed_message=failed_message)
+        if r > 0:
+            time.sleep(sleep_sec)
