4bb6e1fa4dc3078f4325f8ca34de7c38e9c99a59
Pomin Wu <pomin5@gmail.com>
2017-02-21 14:05:30 +0800

    Added data check

--- test/data/test_latlon.py
+++ test/data/test_latlon.py
@@ -12,7 +12,6 @@ from crawler.logging import default_logger as logger
 
 class LatLonTest(unittest.TestCase):
 
-    @unittest.expectedFailure
     def test_aed_latlon_and_place_latlon(self):
         for pid in get_all_saved_ids():
             data = get_data(pid)
@@ -22,6 +21,23 @@ class LatLonTest(unittest.TestCase):
             if data['aed']['AED經緯度']['y'] != data['place']['場所經緯度']['y']:
                 logger.warning(
                     '{pid} has mismatched lat between place and aed'.format(pid=pid))
+
+    def test_place_address_and_place_latlon(self):
+        addresses = {}
+        for pid in get_all_saved_ids():
+            data = get_data(pid)
+            if data['place']['場所地址'] not in addresses:
+                addresses[data['place']['場所地址']] = []
+            latlon =  'x,y'.format(*data['place']['場所經緯度'])
+            if latlon not in addresses[data['place']['場所地址']]:
+                addresses[latlon] = [pid]
+            else:
+                addresses[latlon].append(pid)
+        for addr in addresses.keys():
+            n = len(addresses[addr])
+            if n > 1:
+                logger.warning(
+                    '{addr} has {n} location'.format(addr=addr, n=n))
 
 if __name__ == "__main__":
     unittest.main()
